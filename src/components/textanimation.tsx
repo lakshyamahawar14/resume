ce89ae9e9dec1a061dd09958b1a9dfd5ed2ffdb3
luxdotkom <lakshyamahawar14@gmail.com>
2024-08-03 16:49:10 +0530

    adding buttons and anchors clickable

--- src/components/textanimation.tsx
+++ src/components/textanimation.tsx
@@ -6,11 +6,11 @@ import gsap from "gsap";
 const TextAnimation = () => {
   useEffect(() => {
     const textualElements = document.querySelectorAll(
-      "a, span, p, h1, h2, h3, h4, button, img"
+      "span, p, h1, h2, h3, h4, img"
     );
     gsap.to(textualElements, {
       opacity: 1,
-      duration: 0.2,
+      duration: 0.5,
       stagger: 0,
       ease: "power1.out",
       onComplete() {
@@ -19,6 +19,13 @@ const TextAnimation = () => {
         sectionalElements.forEach((el) => {
           el.classList.add("fade-in");
         });
+
+        const anchorElements = document.getElementsByTagName("a");
+        const buttonElements = document.getElementsByTagName("button");
+        for (var i = 0; i < anchorElements.length; ++i) {
+          anchorElements[i]?.style.setProperty("z-index", "30");
+          buttonElements[i]?.style.setProperty("z-index", "30");
+        }
       },
     });
   });
